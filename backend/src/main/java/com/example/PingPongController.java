1aa1b72a990a83440b0870af2a8fcc9391c6310c
George <georgemagdy370@gmail.com>
2024-04-11 17:07:23 +0200

    CD Workflow with new controller

---
+++ backend/src/main/java/com/example/PingPongController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// This class is for us to test that Continuous Delivery is working fine
+
+@RestController
+public class PingPongController {
+
+    private static int COUNTER = 0;
+
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong: %s".formatted(++COUNTER));
+    }
+}
