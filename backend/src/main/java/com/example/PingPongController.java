cad64b01ed2adbfd238ad8a6a47574fd15daff62
George <georgemagdy370@gmail.com>
2024-04-11 17:40:11 +0200

    CD Workflow with new controller

--- backend/src/main/java/com/example/PingPongController.java
+++ backend/src/main/java/com/example/PingPongController.java
@@ -3,7 +3,9 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 // This class is for us to test that Continuous Delivery is working fine
+
 
 @RestController
 public class PingPongController {
