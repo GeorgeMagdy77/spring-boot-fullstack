f4d7d5cda5baf8a067a8f54bdee670fb76913bf4
George <georgemagdy370@gmail.com>
2024-04-11 23:36:36 +0200

    Add Counter to PingPongController

--- backend/src/main/java/com/example/PingPongController.java
+++ backend/src/main/java/com/example/PingPongController.java
@@ -3,17 +3,17 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-// This class is for us to test that Continuous Delivery is working fine
-
+// This class is for us to test that Continuous Delivery (Deployment with Elastic Beanstalk) is working fine
 
 @RestController
 public class PingPongController {
+
+    private static int COUNTER = 0;
 
     record PingPong(String result){}
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong");
+        return new PingPong("Pong: %s".formatted(++COUNTER));
     }
 }
