0f29136b430cb853abb81523a0ce2dab80281298
George <georgemagdy370@gmail.com>
2024-05-16 00:03:57 +0300

    Dockerise react frontend

--- backend/src/main/java/com/example/PingPongController.java
+++ backend/src/main/java/com/example/PingPongController.java
@@ -1,7 +1,7 @@
 package com.example;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 // This class is for us to test that Continuous Delivery (Deployment with Elastic Beanstalk) is working fine
 
